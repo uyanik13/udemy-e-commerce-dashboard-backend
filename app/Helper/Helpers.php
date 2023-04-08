3700f3965433c42b2b1dd949c04c2fb49dc591dc
Oğur UYANIK <ogur.uyanik@pfizer.com>
2023-04-09 00:35:30 +0300

    added helper and defined image upload function

---
+++ app/Helper/Helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+
+
+if(!function_exists('uploadImage')){
+    function uploadImage(UploadedFile $file, string $path, string $imageName = null)
+    {
+        $extension = $file->getClientOriginalExtenstion();
+        $imageName = $imageName?? uniqid();
+        $fileName = $imageName . '.' . $extension;
+        Storage::disk('public')->putFileAs('images/'. $path, $file, $fileName);
+        $imageUrl = env('APP_URL') . '/images/' . $path . '/' . $fileName;
+        return [
+            'image_url' => $imageUrl,
+            'image_type' => $extension
+        ];
+    }
+}
